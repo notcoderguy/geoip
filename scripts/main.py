39d88b4e8d34096380ee815403dffd57fd84e690
NotCoderGuy <notcoderguy@gmail.com>
2024-01-15 21:09:31 +0530

    Removed Bloat & added mmdb files

--- scripts/main.py
+++ scripts/main.py
@@ -5,14 +5,14 @@ from urllib.parse import urlparse
 from dotenv import dotenv_values
 import redis
 
-def download_mmdb_files(LICENSE_KEY):
+def download_mmdb_files(LICENSE_KEY, MMDB_DIRECTORY):
     LINKS = [
         'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key={}&suffix=tar.gz'.format(LICENSE_KEY),
         'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key={}&suffix=tar.gz'.format(LICENSE_KEY),
         'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key={}&suffix=tar.gz'.format(LICENSE_KEY),
     ]
 
-    download_dir = os.path.join(os.getcwd(), './../storage/app/mmdb/')
+    download_dir = os.path.join(os.getcwd(), MMDB_DIRECTORY)
     os.makedirs(download_dir, exist_ok=True)
 
     for link in LINKS:
@@ -21,8 +21,8 @@ def download_mmdb_files(LICENSE_KEY):
             check=True
         )
 
-def unzip_mmdb_files():
-    download_dir = os.path.join(os.getcwd(), './../storage/app/mmdb/')
+def unzip_mmdb_files(MMDB_DIRECTORY):
+    download_dir = os.path.join(os.getcwd(), MMDB_DIRECTORY)
     for filename in os.listdir(download_dir):
         if filename.endswith('.tar.gz'):
             subprocess.run(
@@ -30,14 +30,14 @@ def unzip_mmdb_files():
                 check=True
             )
 
-def clean_up_mmdb_files():
-    download_dir = os.path.join(os.getcwd(), './../storage/app/mmdb/')
+def clean_up_mmdb_files(MMDB_DIRECTORY):
+    download_dir = os.path.join(os.getcwd(), MMDB_DIRECTORY)
     for filename in os.listdir(download_dir):
         if filename.endswith('.tar.gz'):
             os.remove(os.path.join(download_dir, filename))
 
-def move_mmdb_files():
-    search_dir = os.path.join(os.getcwd(), './../storage/app/mmdb/')
+def move_mmdb_files(MMDB_DIRECTORY):
+    search_dir = os.path.join(os.getcwd(), MMDB_DIRECTORY)
     for folder in os.listdir(search_dir):
         if folder.startswith('GeoLite2'):
             folder_path = os.path.join(search_dir, folder)
@@ -51,8 +51,8 @@ def move_mmdb_files():
             # Remove the empty subdirectory
             shutil.rmtree(folder_path)
 
-def caching_mmdb_files():
-    search_dir = os.path.join(os.getcwd(), './../storage/app/mmdb/')
+def caching_mmdb_files(MMDB_DIRECTORY):
+    search_dir = os.path.join(os.getcwd(), MMDB_DIRECTORY)
     for file in os.listdir(search_dir):
         if file.endswith('.mmdb'):
             file_path = os.path.join(search_dir, file)
@@ -66,14 +66,16 @@ def main():
     config = dotenv_values(".env")
     print('[+] Copying License Key from .env file...')
     license_key = config['LICENSE_KEY']
+    print('[+] Copying MMDB folder path.')
+    mmdb_directory = config['']
     print('[+] Downloading mmdb files...')
-    download_mmdb_files(license_key)
+    download_mmdb_files(license_key, mmdb_directory)
     print('[+] Unzipping mmdb files...')
-    unzip_mmdb_files()
+    unzip_mmdb_files(mmdb_directory)
     print('[+] Cleaning up mmdb files...')
-    clean_up_mmdb_files()
+    clean_up_mmdb_files(mmdb_directory)
     print('[+] Moving mmdb files...')
-    move_mmdb_files()
+    move_mmdb_files(mmdb_directory)
     # This is not working in laravel, need to figure out how to cache the files
     # print('[+] Caching mmdb files...')
     # caching_mmdb_files()
