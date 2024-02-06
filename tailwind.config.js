a99710bf0fab929019dccfd1776f23ba9c5e23ca
NotCoderGuy <notcoderguy@gmail.com>
2024-02-06 07:47:43 +0530

    Updated UI configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './vendor/laravel/jetstream/**/*.blade.php',
+    './storage/framework/views/*.php',
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
-    "./resources/**/*.vue",
+    "./resources/**/*.vue"
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      colors: {
-        'text': '#17150f',
-        'background': '#FEFBF5',
-        'primary': '#043B4F',
-        'secondary': '#032B3A',
-        'tertiary' : '#054962'
-       },       
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: [
+      {
+        dracula : {
+          ...require("daisyui/src/theming/themes")["dracula"],
+        },
+      },
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: false, // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 }
 
