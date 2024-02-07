fdfe7e2b64dac22ef4f686cbcfdf34cd9dbe63aa
NotCoderGuy <notcoderguy@gmail.com>
2024-02-07 09:53:31 +0530

    Updated docs styling

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.base')
+@section('title', 'Documentation')
 
 @section('body')
     <div class="bg-base-200 flex min-h-screen">
@@ -43,7 +44,7 @@
 
             <!-- Section 1: Getting Started -->
             <section id="getting-started" class="mb-8">
-                <h2 class="text-secondary text-3xl font-medium mb-4 underline decoration-dashed"># Getting Started</h2>
+                <h2 class="text-secondary text-3xl font-medium mb-4"># Getting Started</h2>
                 <p>Welcome to our geolocation API, designed specifically for webmasters and website owners. Our service
                     enables you to geo-locate your users, offering them customized data for an enhanced browsing experience.
                     Start leveraging our API today to track user locations and deliver the best possible results—no signup
@@ -52,7 +53,7 @@
 
             <!-- Section 2: API -->
             <section id="api" class="mb-8">
-                <h2 class="text-secondary text-3xl font-medium mb-4 underline decoration-dashed"># API</h2>
+                <h2 class="text-secondary text-3xl font-medium mb-4"># API</h2>
                 <p>Our API grants access to user location data, including the country flag, language, and currency, to
                     provide tailored content and services. Utilize our endpoints:</p>
                 <ul>
@@ -64,7 +65,7 @@
 
             <!-- Section 3: Rate Limiting -->
             <section id="rate-limiting" class="mb-8">
-                <h2 class="text-secondary text-3xl font-medium mb-4 underline decoration-dashed"># Rate Limiting</h2>
+                <h2 class="text-secondary text-3xl font-medium mb-4"># Rate Limiting</h2>
                 <p>Our API is designed to accommodate a wide range of requests without stringent rate limits. In the rare
                     event of excessive requests, such as a DDos attack, we ensure stability by rate limiting to protect our
                     services.</p>
@@ -72,28 +73,28 @@
 
             <!-- Section 4: Error Handling -->
             <section id="error-handling" class="mb-8">
-                <h2 class="text-secondary text-3xl font-medium mb-4 underline decoration-dashed"># Error Handling</h2>
+                <h2 class="text-secondary text-3xl font-medium mb-4"># Error Handling</h2>
                 <p>Our system is built to manage errors efficiently, ensuring you receive optimal output. In case of an
                     incorrect IP format, the API will respond with an error, guiding you to correct the request.</p>
             </section>
 
             <!-- Section 5: Privacy Policy -->
             <section id="privacy-policy" class="mb-8">
-                <h2 class="text-secondary text-3xl font-medium mb-4 underline decoration-dashed"># Privacy Policy</h2>
+                <h2 class="text-secondary text-3xl font-medium mb-4"># Privacy Policy</h2>
                 <p>Your privacy is paramount. Our service does not log or collect any user data, ensuring full anonymity and
                     security in your use of our API.</p>
             </section>
 
             <!-- Section 6: Terms of Service -->
             <section id="terms-of-service" class="mb-8">
-                <h2 class="text-secondary text-3xl font-medium mb-4 underline decoration-dashed"># Terms of Service</h2>
+                <h2 class="text-secondary text-3xl font-medium mb-4"># Terms of Service</h2>
                 <p>By using our service, you agree to our terms, crafted to ensure a safe and reliable experience. While we
                     impose no specific restrictions, we encourage responsible use of our API.</p>
             </section>
 
             <!-- Section 7: Contact -->
             <section id="contact" class="mb-8">
-                <h2 class="text-secondary text-3xl font-medium mb-4 underline decoration-dashed"># Contact</h2>
+                <h2 class="text-secondary text-3xl font-medium mb-4"># Contact</h2>
                 <p>For support or inquiries, reach out directly on Discord (@notcoderguy). Please include all relevant
                     details in your initial message to ensure a swift and accurate response.</p>
             </section>
