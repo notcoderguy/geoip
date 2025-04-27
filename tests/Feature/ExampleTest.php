669b853a14f31a7c902a1adac19f9b842017c78a
Vasu Grover <notcoderguy@gmail.com>
2025-04-27 20:23:18 +0530

    feat: udpate test and removed redundant ones

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,34 @@
 <?php
 
-it('returns a successful response', function () {
+it('returns a successful response for the root URL', function () {
     $response = $this->get('/');
+
+    $response->assertStatus(200);
+});
+
+it('returns a successful response for the API root', function () {
+    $response = $this->get('/api');
+
+    $response->assertStatus(200)
+        ->assertJsonStructure([
+            'message',
+            'version',
+            'License',
+            'website',
+            'documentation',
+            'github',
+            'author',
+        ]);
+});
+
+// it('returns a successful response for the detect API', function () {
+//     $response = $this->get('/api/detect');
+
+//     $response->assertStatus(200);
+// });
+
+it('returns a successful response for the detect IP API', function () {
+    $response = $this->get('/api/detect/8.8.8.8');
 
     $response->assertStatus(200);
 });
